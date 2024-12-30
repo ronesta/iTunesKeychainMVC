7e75defc597964841635d2180b7639b817ecaff7
ronesta <gvozzzd123@gmail.com>
2024-12-30 15:18:32 +0300

    Project without architecture

---
+++ iTunesKeychainMVC/iTunesKeychainMVC/ImageLoader.swift
@@ -0,0 +1,54 @@
+//
+//  ImageLoader.swift
+//  iTunesKeychainMVC
+//
+//  Created by Ибрагим Габибли on 30.12.2024.
+//
+
+import Foundation
+import UIKit
+
+final class ImageLoader {
+    static let shared = ImageLoader()
+    private init() {}
+    var counter = 1
+
+    func loadImage(from urlString: String, completion: @escaping (UIImage?) -> Void) {
+
+        if let imageData = KeychainSevice.shared.loadImage(key: urlString),
+           let image = UIImage(data: imageData) {
+            completion(image)
+            return
+        }
+
+        guard let url = URL(string: urlString) else {
+            completion(nil)
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error {
+                print("Error: \(error.localizedDescription)")
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+                return
+            }
+
+            if let data,
+               let image = UIImage(data: data) {
+                KeychainSevice.shared.saveImage(data, key: urlString)
+                DispatchQueue.main.async {
+                    completion(image)
+                }
+                print("Load image", self.counter)
+                self.counter += 1
+            } else {
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+            }
+        }.resume()
+    }
+}
+
